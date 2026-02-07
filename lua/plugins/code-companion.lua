0c6465455dc733b8ed77edc8d4a7507f3a54659a
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-02-07 01:47:53 +0100

    added code companion

---
+++ lua/plugins/code-companion.lua
@@ -0,0 +1,10 @@
+return {
+	"olimorris/codecompanion.nvim",
+	enabled = true,
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {},
+}
