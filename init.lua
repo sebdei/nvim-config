85d9de8ecfaab8a3a4fd4a2887c8ba461debe229
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-04-12 12:47:53 +0200

    remove comment

--- init.lua
+++ init.lua
@@ -28,24 +28,4 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	-- automatically check for plugin updates
 	checker = { enabled = true },
-}, {
-	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		-- icons = vim.g.have_nerd_font and {} or {
-		-- 	cmd = "⌘",
-		-- 	config = "🛠",
-		-- 	event = "📅",
-		-- 	ft = "📂",
-		-- 	init = "⚙",
-		-- 	keys = "🗝",
-		-- 	plugin = "🔌",
-		-- 	runtime = "💻",
-		-- 	require = "🌙",
-		-- 	source = "📄",
-		-- 	start = "🚀",
-		-- 	task = "📌",
-		-- 	lazy = "💤 ",
-		-- },
-	},
 })
