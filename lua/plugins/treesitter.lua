68dce592f9ed811b981f56aab976ff2fdf8fc248
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-04-12 13:42:12 +0200

    mac

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,12 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local supported_languages = { "go", "javascript", "lua" }
+		local supported_languages = { "go", "javascript", "lua", "vue" }
 
 		require("nvim-treesitter").install(supported_languages)
 
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "go" },
+			pattern = supported_languages,
 			callback = function()
 				vim.treesitter.start()
 			end,
