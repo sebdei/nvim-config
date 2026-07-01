0a109440716e866b141ace379fd35ddfab00ca74
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-07-01 21:46:05 +0200

    added yaml

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local supported_languages = { "go", "java", "javascript", "lua", "vue" }
+		local supported_languages = { "go", "java", "javascript", "lua", "vue", "yaml" }
 
 		require("nvim-treesitter").setup({
 			ensure_installed = supported_languages,
