7952d1fc8bfb6c7ea7294fcc5e22e5735d5ccd34
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-09-05 21:48:32 +0200

    open minifiles with \

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -2,7 +2,22 @@ return {
 	"nvim-mini/mini.files",
 	version = "*",
 	keys = {
-		{ "\\", ":lua MiniFiles.open()<CR>", desc = "Minifiles open", silent = true },
+		{
+			"\\",
+			function()
+				local MiniFiles = require("mini.files")
+
+				local path = vim.api.nvim_buf_get_name(0)
+				local isValidPath = vim.fn.filereadable(path) == 1
+
+				if isValidPath then
+					MiniFiles.open(path)
+				else
+					MiniFiles.open()
+				end
+			end,
+			desc = "Minifiles open at current buffer",
+		},
 	},
 	config = function()
 		require("mini.files").setup()
