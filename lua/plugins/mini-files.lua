73b539092f57d61e5b941f657825ee7a6059fb1e
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-09-05 23:11:15 +0200

    optimized mini.files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -4,22 +4,47 @@ return {
 	keys = {
 		{
 			"\\",
+			-- Toggle mini.files: reopen last location, or sync to current buffer
 			function()
 				local MiniFiles = require("mini.files")
 
-				local path = vim.api.nvim_buf_get_name(0)
-				local isValidPath = vim.fn.filereadable(path) == 1
+				local state = MiniFiles.get_explorer_state()
+				local buf = vim.api.nvim_buf_get_name(0)
 
-				if isValidPath then
-					MiniFiles.open(path)
+				if state then
+					local target = buf
+
+					if vim.bo.filetype == "minifiles" then
+						local tw = state.target_window
+
+						if vim.api.nvim_win_is_valid(tw) then
+							target = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(tw))
+						end
+					end
+
+					local isValidFile = target ~= "" and vim.fn.filereadable(target) == 1
+
+					if isValidFile then
+						MiniFiles.open(target, false)
+					else
+						MiniFiles.open()
+					end
 				else
-					MiniFiles.open()
+					local latest = MiniFiles.get_latest_path()
+
+					local isFile = vim.fn.filereadable(buf) == 1
+					local isDirectory = vim.fn.isdirectory(buf) == 1
+
+					if latest then
+						MiniFiles.open(latest)
+					elseif isFile or isDirectory then
+						MiniFiles.open(buf, false)
+					else
+						MiniFiles.open()
+					end
 				end
 			end,
-			desc = "Minifiles open at current buffer",
+			desc = "Sync mini.files to current buffer, or reopen last location",
 		},
 	},
-	config = function()
-		require("mini.files").setup()
-	end,
 }
