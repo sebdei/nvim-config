787e9151691256760c9834818a068f835b3a8754
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-09-06 21:49:03 +0200

    opencode fixes; added search

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -8,33 +8,29 @@ return {
 			-- Your configuration, if any; goto definition on the type for details
 		}
 
-		local function scroll(buf)
-			vim.keymap.set("n", "<S-Up>", function()
-				require("opencode").command("session.half.page.up")
-			end, { buffer = buf, desc = "Scroll OpenCode up" })
-			vim.keymap.set("n", "<S-Down>", function()
-				require("opencode").command("session.half.page.down")
-			end, { buffer = buf, desc = "Scroll OpenCode down" })
-		end
-
-		vim.api.nvim_create_autocmd("TermOpen", {
-			callback = function(event)
-				if vim.api.nvim_buf_get_name(event.buf):match("opencode") then
-					scroll(event.buf)
-				end
-			end,
-		})
+		vim.keymap.set("n", "<S-Up>", function()
+			require("opencode").command("session.half.page.up")
+		end, { desc = "Scroll OpenCode up" })
+		vim.keymap.set("n", "<S-Down>", function()
+			require("opencode").command("session.half.page.down")
+		end, { desc = "Scroll OpenCode down" })
 	end,
+
 	keys = {
 		{ "<leader>co", '<Cmd>lua require("opencode").ask("@this: ")<CR>', mode = { "n", "x" }, desc = "Ask OpenCode" },
 		{ "<leader>cx", '<Cmd>lua require("opencode").select()<CR>', mode = { "n", "x" }, desc = "Select OpenCode" },
-		{ "<leader>cs", '<Cmd>lua vim.env.OPENCODE_ENABLE_EXA = "1"; require("opencode").ask("Search the web for: ")<CR>', mode = { "n", "x" }, desc = "OpenCode web search" },
+		{
+			"<leader>cs",
+			'<Cmd>lua vim.env.OPENCODE_ENABLE_EXA = "1"; require("opencode").ask("Search the web for: ")<CR>',
+			mode = { "n", "x" },
+			desc = "OpenCode web search",
+		},
 		{
 			"<leader>cr",
 			function()
 				return require("opencode").operator("@this ")
 			end,
-			mode = "n",
+			mode = { "n", "x" },
 			expr = true,
 			desc = "Append range to OpenCode",
 		},
