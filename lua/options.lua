0f09fabb52953d5f79410ec069f770e8c93f23bf
Sebastian Deisel <sebastian.deisel@gmail.com>
2026-09-06 21:48:03 +0200

    force showmode

--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,7 @@ vim.opt.termguicolors = true
 
 vim.o.number = true
 vim.o.relativenumber = true
--- vim.o.showmode = false
+vim.o.showmode = true
 
 vim.schedule(function()
 	vim.o.clipboard = "unnamedplus"
